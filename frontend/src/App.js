c518e82ca7ebe7ec4efd9f4151c095a403f0fedc
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2022-03-03 18:35:21 +0100

    Added tbody to Card table. Made Card component dynamic. Moved Card component into the App component. Passing player data to Card component through props from JSON file Players.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import Card from "./components/Card";
+import { Players } from "./Players";
 
-const App = () => {
-  return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = Players[0];
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <div className="App">
+        <h1>Hello world!</h1>
+        <Card
+          player={this.state}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,61 +1,84 @@
+import React from "react";
 import "./Card.css";
 
-const Card = () => {
-  return (
-    <div className="card-container">
-      <div className="rectangle">
-        <div className="top-container">
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="card-container">
+        <div className="rectangle">
+          <div className="top-container">
             <div className="wrap">
-          <div className="left-top">
-            <div className="rating-container">
-              <p className="rating">97</p>
-              <p className="position">CAM</p>
+              <div className="left-top">
+                <div className="rating-container">
+                  <p className="rating">{this.props.player.rating}</p>
+                  <p className="position">{this.props.player.position.toUpperCase()}</p>
+                </div>
+                <div className="country-container">
+                  <div className="country-flag"></div>
+                </div>
+              </div>
+              <div className="stadium">
+                <div className="right-top">
+                  <div className="space-top"></div>
+                  <div className="player-head"></div>
+                </div>
+              </div>
             </div>
-            <div className="country-container">
-              <div className="country-flag"></div>
-            </div>
+            <div className="separador1"></div>
           </div>
-          <div className="stadium">
-            <div className="right-top">
-              <div className="space-top"></div>
-              <div className="player-head"></div>
-            </div>
-          </div>
-          </div>
-          <div className="separador1"></div>
-        </div>
-        <div className="names-container">
+          <div className="names-container">
             <div className="wrap2">
-          <h3 className="player-name">MARADONA</h3>
-          </div>
-          <div className="separador2"></div>
-        </div>
-        <div className="field">
-          <div className="bottom-container">
-            <div className="space-top-stats"></div>
-            <div className="stats">
-              <table>
-                <tr>
-                  <td>97 <span className="att-stat">PAC</span></td>
-                  <td>97 <span className="att-stat">DRI</span></td>
-                </tr>
-                <tr>
-                  <td>93 <span className="att-stat">SHO</span></td>
-                  <td>40 <span className="att-stat">DEF</span></td>
-                </tr>
-                <tr>
-                  <td>92 <span className="att-stat">PAS</span></td>
-                  <td>76 <span className="att-stat">PHY</span></td>
-                </tr>
-              </table>
+              <h3 className="player-name">{this.props.player.name.toUpperCase()}</h3>
             </div>
-            <div className="logo-container">
-              <div className="logo"></div>
+            <div className="separador2"></div>
+          </div>
+          <div className="field">
+            <div className="bottom-container">
+              <div className="space-top-stats"></div>
+              <div className="stats">
+                <table>
+                  <tbody>
+                    <tr>
+                      <td>
+                        {this.props.player.pace} <span className="att-stat">PAC</span>
+                      </td>
+                      <td>
+                        {this.props.player.dribbling}{" "}
+                        <span className="att-stat">DRI</span>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        {this.props.player.shooting}{" "}
+                        <span className="att-stat">SHO</span>
+                      </td>
+                      <td>
+                        {this.props.player.defense}{" "}
+                        <span className="att-stat">DEF</span>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        {this.props.player.passing}{" "}
+                        <span className="att-stat">PAS</span>
+                      </td>
+                      <td>
+                        {this.props.player.physical}{" "}
+                        <span className="att-stat">PHY</span>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div className="logo-container">
+                <div className="logo"></div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
 export default Card;
