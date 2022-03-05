7c993657d446b85a720b0d4fbd4d341e4a0a0ec1
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2022-03-05 14:47:20 +0100

    Changed classes to functions. Removed button to switch between players. Show all the players using map function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,43 +2,21 @@ import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 import Card from "./components/Card";
-import players from "./data/players.json"
+import Players from "./data/players.json";
 
-let Players = []
-players.forEach(player => {
-  Players.push(player)
+let players = [];
+Players.forEach((player) => {
+  players.push(player);
 });
 
-class Button extends React.Component {
-  render() {
-    return (
-      <div>
-        <button onClick={() => this.props.onClick()}>Cambiar jugador</button>
-      </div>
-    );
-  }
-}
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = Players[0];
-  }
-
-  handleClick() {
-    this.setState(Players[this.state.id]);
-  }
-
-  render() {
-    console.log(this.state);
-    return (
-      <div className="App">
-        <h1>Hello world!</h1>
-        <Card player={this.state} />
-        <Button onClick={() => this.handleClick()} />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      {players.map((player) => (
+        <Card player={player} />
+      ))}
+    </div>
+  );
+};
 
 export default App;
--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,96 +1,98 @@
 import React from "react";
 import "./Card.css";
 
-class Card extends React.Component {
-  render() {
-    return (
-      <div className="card-container">
-        <div className="rectangle">
-          <div className="top-container">
-            <div className="wrap">
-              <div className="left-top">
-                <div className="con-1">
+const Card = ({ player }) => {
+  return (
+    <div className="card-container" key={ player.id }>
+      <div className="rectangle">
+        <div className="top-container">
+          <div className="wrap">
+            <div className="left-top">
+              <div className="con-1">
                 <div className="con-2">
-                <div className="rating-container">
-                  <p className="rating">{this.props.player.rating}</p>
-                  <p className="position">
-                    {this.props.player.position.toUpperCase()}
-                  </p>
-                </div>
-                <div className="country-container">
-                  <div className="country-flag" style={{ backgroundImage: `url("https://countryflagsapi.com/png/${this.props.player.country}")` }} ></div>
-                </div>
-                </div>
-                <div className="separador3"></div>
-                </div>
-                <div className="separador1-a"></div>
-              </div>
-              <div className="stadium">
-                <div className="right-top">
-                  <div className="space-top"></div>
-                  <div className="player-head" style={{ backgroundImage: `url(${this.props.player.img})` }}>
-                    <div className="separador1-b"></div>
+                  <div className="rating-container">
+                    <p className="rating">{player.rating}</p>
+                    <p className="position">
+                      {player.position.toUpperCase()}
+                    </p>
+                  </div>
+                  <div className="country-container">
+                    <div
+                      className="country-flag"
+                      style={{ backgroundImage: `url("https://countryflagsapi.com/png/${player.country}")` }}
+                    >
+                    </div>
                   </div>
                 </div>
+                <div className="separador3"></div>
               </div>
+              <div className="separador1-a"></div>
             </div>
-          </div>
-          <div className="names-container">
-            <div className="wrap2">
-              <h3 className="player-name">
-                {this.props.player.name.toUpperCase()}
-              </h3>
-            </div>
-            <div className="separador2"></div>
-          </div>
-          <div className="field">
-            <div className="bottom-container">
-              <div className="space-top-stats"></div>
-              <div className="stats">
-                <table>
-                  <tbody>
-                    <tr>
-                      <td>
-                        {this.props.player.pace}{" "}
-                        <span className="att-stat">PAC</span>
-                      </td>
-                      <td>
-                        {this.props.player.dribbling}{" "}
-                        <span className="att-stat">DRI</span>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        {this.props.player.shooting}{" "}
-                        <span className="att-stat">SHO</span>
-                      </td>
-                      <td>
-                        {this.props.player.defense}{" "}
-                        <span className="att-stat">DEF</span>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        {this.props.player.passing}{" "}
-                        <span className="att-stat">PAS</span>
-                      </td>
-                      <td>
-                        {this.props.player.physical}{" "}
-                        <span className="att-stat">PHY</span>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-              <div className="logo-container">
-                <div className="logo"></div>
+            <div className="stadium">
+              <div className="right-top">
+                <div className="space-top"></div>
+                <div
+                  className="player-head"
+                  style={{ backgroundImage: `url(${player.img})` }}
+                >
+                  <div className="separador1-b"></div>
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <div className="names-container">
+          <div className="wrap2">
+            <h3 className="player-name">{player.name.toUpperCase()}</h3>
+          </div>
+          <div className="separador2"></div>
+        </div>
+        <div className="field">
+          <div className="bottom-container">
+            <div className="space-top-stats"></div>
+            <div className="stats">
+              <table>
+                <tbody>
+                  <tr>
+                    <td>
+                      {player.pace} <span className="att-stat">PAC</span>
+                    </td>
+                    <td>
+                      {player.dribbling}{" "}
+                      <span className="att-stat">DRI</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      {player.shooting}{" "}
+                      <span className="att-stat">SHO</span>
+                    </td>
+                    <td>
+                      {player.defense}{" "}
+                      <span className="att-stat">DEF</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      {player.passing}{" "}
+                      <span className="att-stat">PAS</span>
+                    </td>
+                    <td>
+                      {player.physical}{" "}
+                      <span className="att-stat">PHY</span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="logo-container">
+              <div className="logo"></div>
+            </div>
+          </div>
+        </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Card;
